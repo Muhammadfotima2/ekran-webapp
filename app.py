e0128a9c5bbf121ed584c1e3fed6c5c6aba2394f
Muhammadfotima2 <Muhammadfotima24@gmail.com>
2025-07-10 09:39:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, send_from_directory
 import os
 
-app = Flask(name, static_folder='public')
+app = Flask(__name__, static_folder='public')
 
 @app.route('/')
 def catalog():
-    return send_from_directory('public', 'catalog.html')
+    return send_from_directory('public', 'admin.html')
 
-if name == 'main':
+if __name__ == '__main'__:
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=port)
